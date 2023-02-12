accbf4bb1db2aa018b737f11f49415183869b5cd
Jacob Snarr <jacobsnarr@gmail.com>
2023-02-12 01:28:56 -0500

    Design message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,10 +1,21 @@
-function Message(props: { author: String, children: String }) {
-  return <div className="w-full bg-white rounded-3xl h-fit px-6 py-4 text-2xl shadow-md">
-    <div className="font-bold">
-      {props.author}
+import { User } from "@supabase/supabase-js";
+import { MessageObject, UserObject } from "../utils/supabaseClient";
+
+function Message(props: { message: MessageObject }) {
+  return <div className="w-full bg-white rounded-3xl h-fit px-5 py-3 text-lg shadow-md">
+    <div>
+      <span className="font-bold">
+        {(props.message.user as UserObject).name}
+      </span>
+      <span className="italic ml-2 font-thin text-base">
+        {(props.message.user as UserObject).pronouns}
+      </span>
+      <span className="ml-2 text-base font-thin float-right">
+        {new Date(props.message.created_at).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}
+      </span>
     </div>
     <div>
-      {props.children}
+      {props.message.content}
     </div>
   </div>
 }
