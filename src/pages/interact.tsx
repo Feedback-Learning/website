fda6551f141334d698942593e94265371736569f
Jacob Snarr <jacobsnarr@gmail.com>
2023-02-12 01:30:18 -0500

    Add reaction submissions

--- src/pages/interact.tsx
+++ src/pages/interact.tsx
@@ -1,20 +1,32 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "@supabase/auth-helpers-react";
 import { Layout } from "../components/Layout";
 import Reaction from "../components/Reaction";
-import Message from "../components/ChatInput";
 import Chat from "../components/Chat";
+import { MessageObject, supabase } from "../utils/supabaseClient";
+import { useRouter } from "next/router";
 
-const Home: NextPage = () => {
-        
-  const session = useSession()
+const Home: NextPage = () => {  
+
+  const session = useSession();
+
+  const router = useRouter();
+  
+  useEffect(() => {
+    
+  })
+
+  async function sendMessage(messageObject: MessageObject) {
+    let {data, error} = await supabase.from('messages').insert(messageObject)
+    console.log(data, error)
+  }
 
   return (
     <>
-      <Layout session={session}>
-        <div className="flex flex-col justify-end items-center w-full h-full pl-10 pb-10 pr-10 gap-4 overflow-hidden">
+      <Layout>
+        <div className="flex flex-col justify-end items-center w-full h-full pl-6 pb-6 pr-6 gap-4 overflow-hidden">
           <Chat/>
           <div className="flex flex-row justify-between gap-2 w-full">
             <Reaction>👍</Reaction>
