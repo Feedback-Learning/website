81b9256dbef95564bf645b357dc81a32e1e471b1
Jacob Snarr <jacobsnarr@gmail.com>
2023-02-12 06:52:27 -0500

    Remove unused code from interact

--- src/pages/interact.tsx
+++ src/pages/interact.tsx
@@ -1,32 +1,16 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
-import { useSession } from "@supabase/auth-helpers-react";
 import { Layout } from "../components/Layout";
 import Reaction from "../components/Reaction";
 import Chat from "../components/Chat";
-import { MessageObject, supabase } from "../utils/supabaseClient";
-import { useRouter } from "next/router";
 
 const Home: NextPage = () => {  
-
-  const session = useSession();
-
-  const router = useRouter();
-  
-  useEffect(() => {
-    
-  })
-
-  async function sendMessage(messageObject: MessageObject) {
-    let {data, error} = await supabase.from('messages').insert(messageObject)
-    console.log(data, error)
-  }
 
   return (
     <>
       <Layout>
-        <div className="flex flex-col justify-end items-center w-full h-full pl-6 pb-6 pr-6 gap-4 overflow-hidden">
+        <div className="flex flex-col justify-end items-center w-full h-full gap-4 overflow-hidden">
           <Chat/>
           <div className="flex flex-row justify-between gap-2 w-full">
             <Reaction>👍</Reaction>
