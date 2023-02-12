b024e0b5a7572e77275773ba6b067584361e44c1
Jacob Snarr <jacobsnarr@gmail.com>
2023-02-12 01:26:59 -0500

    Add objects to describe database API responses

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, User } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
@@ -6,5 +6,30 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing API keys for Supabase')
 }
+
+export interface MessageObject {
+  class: number,
+  content: string,
+  created_at: string,
+  id: number,
+  is_reaction: boolean,
+  user: string | UserObject,
+}
+
+export interface PayloadObject {
+  commit_timestamp: string,
+  eventType: string,
+  new: MessageObject,
+  old: MessageObject,
+  schema: String,
+  table: String
+}
+
+export interface UserObject {
+  id: string,
+  name: string,
+  pronouns: string
+}
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
